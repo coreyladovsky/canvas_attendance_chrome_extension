8593f4c5b5473614a9924b51da40f23575974417
Corey Ladovsky <coreyskymusic@gmail.com>
2020-06-03 20:57:25 -0400

    fills all rubrics with a 3

--- content.js
+++ content.js
@@ -4,22 +4,24 @@ let visited = new Set();
 
     
     const run = () => {
-        console.log("ran")
         let student = document.querySelector('.ui-selectmenu-item-header').innerText
-        console.log(student)
-        console.log(visited)
+        // if(visited.size === 3) return
         if(visited.has(student)) return; 
         visited.add(student);
-        let rubric = document.querySelector('.toggle_full_rubric');
-        rubric.click();
-        document.querySelector("#grading-box-extended").value = 3;
-
+        document.querySelector("#grade_container").click();
         setTimeout(() => {
-            document.querySelector('.rating-tier').click()
-            document.querySelector('.save_rubric_button').click()
-            document.querySelector(".icon-arrow-right").click();
-            run()
-        }, 1000)
+            let rubric = document.querySelector('.toggle_full_rubric');
+            rubric.click();
+            
+            setTimeout(() => {
+                document.querySelector('.rating-tier').click()
+                document.querySelector('.save_rubric_button').click()
+                document.querySelector("#grading-box-extended").value = 3;
+                document.querySelector(".icon-arrow-right").click();
+                run()
+            }, 1000)
+
+        }, 2000)
         
         
     }
